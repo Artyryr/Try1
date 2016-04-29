e2150389be504c8cc262399c078b4a98e7f6e57f
Artyryr <ArtyryrF@mail.ru>
2016-04-29 20:47:40 +0300

    Revert "Revert "СourseTask""
    This reverts commit 9da7fede8e5f9cd3a8eefad59fbcc44454529863.

---
+++ DataAccessLayer/Artist.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    [Serializable]
+    public class Artist
+    {
+
+        public Artist()
+        {
+            Pictures = new List<Picture>();
+        }
+        public Artist(string name)
+        {
+            Name = name;
+        }
+
+        [DisplayName("Имя")]
+        public String Name { get; set; }
+
+        [DisplayName("Годы жизни")]
+        public String LifeTime { get; set; }
+        [DisplayName("Национальность")]
+        public String Nationality { get; set; }
+
+
+        public List<Picture> Pictures { get; set; }
+        public List<ArtGenre> ArtGenres { get { return Pictures.Select(x => x.Genre).Distinct().ToList(); } }
+
+    }
+}
---
+++ DataAccessLayer/Museum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    [Serializable]
+    public class Museum
+    {
+        public Museum()
+        {
+            Pictures = new List<Picture>();
+        }
+        [DisplayName("Название")]
+        public string Title { get; set; }
+        [DisplayName("Местонахождение")]
+        public string Place { get; set; }
+        [DisplayName("Картины")]
+        public List<Picture> Pictures { get; set; }
+        [DisplayName("Кол-во картин")]
+        public int PicrutesCount { get { return Pictures.Count; } }
+    }
+}
